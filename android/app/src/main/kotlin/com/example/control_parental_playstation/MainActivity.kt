6f1d96558479f3faa417d8692ec092b3a556fc77
TSHIBANGU Kevin <kevintshib08@gmail.com>
2024-10-13 00:59:10 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/control_parental_playstation/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.control_parental_playstation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/settings_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatefulWidget {
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  double _usageDuration = 1; // Durée initiale : 1 heure
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Paramètres de contrôle parental'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Durée maximale d\'utilisation (en heures) : ${_usageDuration.toInt()}h',
+              style: TextStyle(fontSize: 18)),
+            Slider(
+              value: _usageDuration,
+              min: 1,
+              max: 4,
+              divisions: 3,
+              label: '${_usageDuration.toInt()} heures',
+              onChanged: (double value) {
+                setState(() {
+                  _usageDuration = value;
+                });
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Sauvegarder la durée définie et retourner à la page d'accueil
+                Navigator.pop(context, _usageDuration);
+              },
+              child: Text('Sauvegarder'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
